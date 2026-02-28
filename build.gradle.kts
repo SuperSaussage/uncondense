38a730ee0df16a494eb38404ff84aba59b07698b
SuperSaussage <riotblockmc@gmail.com>
2026-02-27 23:24:33 -0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,20 @@
-plugins { java }
+plugins { 
+    java 
+}
 
 group = "com.example"
 version = "1.0.0"
 
 repositories {
     maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://maven.devs.beer/") // 👈 ItemsAdder repo
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
+
+    // 👇 ItemsAdder API (compile only)
+    compileOnly("com.github.LoneDev6:api-itemsadder:3.6.3-beta-14")
 }
 
 java {
